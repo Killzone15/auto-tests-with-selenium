4ede61a01ee7549b88fdb4c0aac1bad7bafb1eae
Killzone15 <killzone15@yandex.ru>
2023-07-24 15:00:45 +0300

    Убраны лишние импорты

--- conftest.py
+++ conftest.py
@@ -3,7 +3,6 @@ import pytest
 from selenium import webdriver
 
 
-
 def pytest_addoption(parser):
     parser.addoption("--language", action="store", default="en",
                      help="Set the language for the tests (default is English)")

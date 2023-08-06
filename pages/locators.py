229a62d8c7bb758c161af8d7b1d927b0ce554b83
Killzone15 <Killzone15@yandex.ru>
2023-08-06 18:33:14 +0300

    Исправлены неверные локаторы для красных тестов, на верные

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class LoginPageLocators:
-    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form1')
-    REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form1')
 
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')

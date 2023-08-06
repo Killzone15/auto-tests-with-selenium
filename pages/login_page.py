b805abf65430c04370d65bbb8eb3e3479e0e232f
Killzone15 <Killzone15@yandex.ru>
2023-08-06 17:48:09 +0300

    Выведины локаторы в отдельный класс для основной страницы (только кнопка логин/регистрация). Для страницы логина и регистрации добавлены локаторы для формы Логина и для формы Регистрации. Реализованы проверки корректного url, проверка налиция формы логина и формы регистрации

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url
+        substring = 'login'
+        full_string = self.browser.curent
+        assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Форма логина не найдена'
+
+
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), 'Форма регистрации не найдена!'
+

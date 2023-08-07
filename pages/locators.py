516fa39006ca14c3d9f3817b599b0788f862addf
Killzone15 <Killzone15@yandex.ru>
2023-08-07 11:06:31 +0300

    Добавлены локаторы для страницы с товаром (ProductPage)

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,8 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators:
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')
+    WRITE_REVIEW = (By.CSS_SELECTOR, '#write_review')

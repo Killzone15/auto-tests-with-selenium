02e77104cf3bd8a3802852d827f5b848e90eb1ae
Killzone15 <killzone15@yandex.ru>
2023-08-11 12:08:28 +0300

    Реализован авто тест для проверки добавления товара в корзину для страницы товара

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,3 +8,14 @@ class ProductPage(BasePage):
         add_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
         add_to_basket.click()
 
+    def the_product_name_matches_the_name_in_the_message(self):
+        product_name = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_MAIN)
+        message_product_name = self.browser.find_element(*ProductPageLocators.NAME_MESSAGE_PRODUCT)
+        assert product_name.text == message_product_name.text, 'Название товара не соотвествует ' \
+                                                     'наименованию товара в сообщении ' \
+                                                     'об успешном добавлении товара в корзину'
+
+    def the_cost_of_the_basket_coincides_with_the_price_of_the_goods(self):
+        price_product_main = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT_MAIN)
+        price_in_message = self.browser.find_element(*ProductPageLocators.PRICE_IN_MESSAGE)
+        assert price_product_main.text == price_in_message.text, 'Цена товара не соотвествует цене товара в сообщении'
--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,14 @@
+import time
+
 from pages.base_page import BasePage
 from pages.product_page import ProductPage
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket()
     BasePage.solve_quiz_and_get_code(product_page)
+    product_page.the_product_name_matches_the_name_in_the_message()
+    product_page.the_cost_of_the_basket_coincides_with_the_price_of_the_goods()

99c721c5029fac29dad3ed024a181ca3e494c466
Killzone15 <killzone15@yandex.ru>
2023-08-13 12:44:17 +0300

    Упрощены текущие локаторы и так же добавлены новый локатор SUCCESS_MESSAGE

--- pages/locators.py
+++ pages/locators.py
@@ -14,7 +14,11 @@ class ProductPageLocators:
     BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')
     WRITE_REVIEW = (By.CSS_SELECTOR, '#write_review')
     PRICE_PRODUCT_MAIN = (By.CSS_SELECTOR, '.col-sm-6.product_main .price_color')
-    PRICE_IN_MESSAGE = (By.CSS_SELECTOR, '#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div '
-                                         '> p:nth-child(1) > strong')
+    PRICE_IN_MESSAGE = (By.CSS_SELECTOR, '#messages > .alert.alert-safe.alert-noicon.alert-info.fade.in '
+                                         '> .alertinner strong')
     NAME_PRODUCT_MAIN = (By.CSS_SELECTOR, '.col-sm-6.product_main h1')
-    NAME_MESSAGE_PRODUCT = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
+    NAME_MESSAGE_PRODUCT = (By.CSS_SELECTOR, '#messages > '
+                                             '.alert.alert-safe.alert-noicon.alert-success.fade.in:nth-child(1) '
+                                             '.alertinner strong')
+
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > .alertinner')

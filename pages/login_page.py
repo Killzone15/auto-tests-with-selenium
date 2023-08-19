3a7ace6a16fcd11b3b40a35291e9ecea3fc99082
Killzone15 <Killzone15@yandex.ru>
2023-08-19 09:43:59 +0300

    Добавлено три теста: 1. Сообщение об успешном добавление карзины отсутствует после добавления в карзину 2. Сообщение об успешном добавлении карзины должно изчезнуть 3. Сообщение об успешном добавлении в карзину должно изчезнуть после добавление товара в карзину.

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,18 +9,13 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url
         substring = 'login'
         full_string = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
         assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Форма логина не найдена!'
 
-
-
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), 'Форма регистрации не найдена!'
-
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), \
+            'Форма регистрации не найдена!'
--- test_product_page.py
+++ test_product_page.py
@@ -10,12 +10,12 @@ from pages.product_page import ProductPage
     'link',
     [
         (f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{i}"
-         if i != 7
-         else pytest.param(
-             f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{i}",
-             marks=pytest.mark.xfail(reason='Название товара не соответствует наименованию товара '
-                                            'в сообщении об успешном добавлении товара в корзину')
-         ))
+        if i != 7
+        else pytest.param(
+            f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{i}",
+            marks=pytest.mark.xfail(reason='Название товара не соответствует наименованию товара '
+                                           'в сообщении об успешном добавлении товара в корзину')
+        ))
         for i in range(10)
     ]
 )
@@ -26,3 +26,28 @@ def test_guest_can_add_product_to_basket(browser, link):
     BasePage.solve_quiz_and_get_code(product_page)
     product_page.the_product_name_matches_the_name_in_the_message()
     product_page.the_cost_of_the_basket_coincides_with_the_price_of_the_goods()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.should_be_disappeared_success_message()
